b07cea9fc51cf25355eef4f5716781a15931b6d2
mohamedkerraz <mohamedreda.kerraz@gmail.com>
2025-02-12 14:56:15 +0100

    add first working generation of queen

---
+++ app.py
@@ -0,0 +1,36 @@
+def set_reines(n):
+    def checkIf_attack(plateau, ligne, col):
+        
+        for i in range(ligne):
+            if plateau[i] == col or \
+               plateau[i] - i == col - ligne or \
+               plateau[i] + i == col + ligne:
+                return False
+        return True
+
+    def process_resolve(plateau, ligne, solutions):
+        
+        solutions.append(plateau[:])
+        for col in range(n):
+            if checkIf_attack(plateau, ligne, col):
+                plateau[ligne] = col
+                process_resolve(plateau, ligne + 1, solutions)
+
+    solutions = []
+    process_resolve([-1] * n, 0, solutions)
+    return solutions
+
+def generate_map(solutions, n):
+    grille = []
+    for sol in solutions:
+        grille = [["#" for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            grille[i][sol[i]] = "R"
+        for ligne in grille:
+            print(" ".join(ligne))
+        print()
+
+n = int(input("Entrez le n: "))
+solutions = set_reines(n)
+
+generate_map(solutions, n)
